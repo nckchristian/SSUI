976f4b6b93150cbcf9ed621abe79bce08388205d
Bmord01 <32937039+Bmord01@users.noreply.github.com>
2018-06-05 18:53:44 -0500

    Update main.c

--- SmartSealzUI/src/main.c
+++ SmartSealzUI/src/main.c
@@ -1028,7 +1028,7 @@ int main(int argc, char *argv[])
 	//=========================================================================
 	GtkCssProvider *provider;
 	provider = gtk_css_provider_new();
-	gtk_css_provider_load_from_path(provider, "style.css",NULL);
+	gtk_css_provider_load_from_path(provider, "src/style.css",NULL);
 	gtk_style_context_add_provider_for_screen(gdk_screen_get_default(), GTK_STYLE_PROVIDER(provider),GTK_STYLE_PROVIDER_PRIORITY_USER);
 
     g_object_unref(builder);
