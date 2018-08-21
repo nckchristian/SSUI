cdb324b875245af0c7933f0010615e22ad5229c1
Bmord01 <32937039+Bmord01@users.noreply.github.com>
2018-08-21 09:54:16 -0500

    Update ShortCutDemo.c

--- SmartSealzUI/Source/ShortCutDemo.c
+++ SmartSealzUI/Source/ShortCutDemo.c
@@ -1,7 +1,10 @@
+/*This prgram drives the logic for the Demo Desktop Shortcut*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 int main(){
-  system("./SB/SSUI/SmartSealzUI/RunSSUI.exe");
+  /*Change master to current folder environment*/
+  //TODO: Create basic form of environment variables
+  system("cd /SB/master/SSUI/SmartSealzUIDemo && ./RunSSUI.exe");
 }
