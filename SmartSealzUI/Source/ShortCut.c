916f29c6211ccc3449ef5cd66d8af98dddc0e20a
Bmord01 <32937039+Bmord01@users.noreply.github.com>
2018-08-21 09:52:34 -0500

    Update ShortCut.c

--- SmartSealzUI/Source/ShortCut.c
+++ SmartSealzUI/Source/ShortCut.c
@@ -1,7 +1,10 @@
+/*This program Drives the logic the shortcut for the desktop*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 int main(){
+  /*Change master to reflect Environment folder*/
+  //TODO: Create basic Environment Variables to replace hardcoding
   system("cd /SB/master/SSUI/SmartSealzUI && ./RunSSUI.exe");
 }
